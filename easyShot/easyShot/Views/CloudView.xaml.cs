71c51036b37f1ca24ee47822718e899b2488cd8e
zh-hang <zzzhhh@bupt.edu.cn>
2020-11-25 12:37:50 +0800

    添加了设置和云端界面

--- easyShot/easyShot/Views/CloudView.xaml.cs
+++ easyShot/easyShot/Views/CloudView.xaml.cs
@@ -13,29 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using easyShot.ViewModels;
-
-namespace easyShot
+namespace easyShot.Views
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// CloudView.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CloudView : UserControl
     {
-        public MainWindow()
+        public CloudView()
         {
             InitializeComponent();
-            DataContext = new SettingViewModel();
-        }
-
-        private void Setting_Click(object sender, RoutedEventArgs e)
-        {
-            DataContext = new SettingViewModel();
-        }
-
-        private void Cloud_Click(object sender, RoutedEventArgs e)
-        {
-            DataContext = new CloudViewModel();
         }
     }
 }
