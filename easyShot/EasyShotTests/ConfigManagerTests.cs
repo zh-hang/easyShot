ade203f3af4ff634ee01dc3ccf9489422294c7af
(buggyminer) <(liaojiudazhongbiao@outlook.com)>
2020-11-25 21:55:26 +0800

    添加单元测试。

---
+++ easyShot/EasyShotTests/ConfigManagerTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EasyShot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyShot.Tests
+{
+    [TestClass()]
+    public class ConfigManagerTests
+    {
+        [TestMethod()]
+        public void ConfigManagerTest()
+        {
+            
+
+        }
+
+        [TestMethod()]
+        public void unitTestTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void loadShotFilePathTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getShotFilePathTest()
+        {
+            ConfigManager configManager = new ConfigManager();
+            EasyShot.ShotMode shotMode = configManager.getShotMode();
+            //System.Console.WriteLine("the shot mode is " + shotMode);
+
+        }
+
+        [TestMethod()]
+        public void setShotFilePathTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void loadStartModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getStartModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setStartModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void loadShotModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void getShotModeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void setShotModeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
