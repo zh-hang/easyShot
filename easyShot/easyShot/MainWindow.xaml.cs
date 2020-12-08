23a90ceb373913b3efb9b10d5542ab47429be3e5
zh-hang <zzzhhh@bupt.edu.cn>
2020-12-08 21:17:49 +0800

    修改了结构

--- easyShot/easyShot/MainWindow.xaml.cs
+++ easyShot/easyShot/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Windows.Data;
 using System.Windows.Controls;
+using System.ComponentModel;
 
 namespace easyShot
 {
@@ -15,68 +16,186 @@ namespace easyShot
     /// </summary>
     /// 
 
-    public class Photo
+    public class Photo : INotifyPropertyChanged
     {
-        public string FullPath { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string fullPath;
+        public string FullPath
+        {
+            get { return fullPath; }
+            set
+            {
+                fullPath = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("FullPath"));
+                }
+            }
+        }
+    }
+
+    public class TextShow : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string account, password, address;
+        public string Account
+        {
+            get { return account; }
+            set
+            {
+                account = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Account"));
+                }
+            }
+        }
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+                }
+            }
+        }
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Address"));
+                }
+            }
+        }
     }
 
 
     public partial class MainWindow : Window
     {
-        SettingViewModel setting;
-        CloudViewModel cloud;
-        public List<Photo> photos = new List<Photo>();
+        private Setting setting;
+        public BindingList<Photo> photos = new BindingList<Photo>();
         public string photos_path;
+        public TextShow textShow;
+
         public MainWindow()
         {
             photos_path = "\\images";
-            getAllImagePath(photos_path);
-            setting = new SettingViewModel();
-            cloud = new CloudViewModel();
             InitializeComponent();
-            DataContext = setting;
-            try
-            {
-                this.lstImg.ItemsSource = photos;
-            }
-            catch (Exception e){ MessageBox.Show(e.Message); };
-
+            textShow = new TextShow();
+            textShow.Account = "123456";
+            textShow.Address = "123456";
+            textShow.Password = "123456";
+            photosInit();
         }
 
-        private void Setting_Click(object sender, RoutedEventArgs e)
-        {
-            DataContext = setting;
-        }
+        //private void Cloud_Click(object sender, RoutedEventArgs e)
+        //{
+        //    DataContext = cloud;
+        //}
 
-        private void Cloud_Click(object sender, RoutedEventArgs e)
-        {
-            DataContext = cloud;
-        }
+        
 
-        public void getAllImagePath(string path)
+        private void getAllImagePath()
         {
-            DirectoryInfo di = new DirectoryInfo(path);
+            DirectoryInfo di = new DirectoryInfo(photos_path);
             FileInfo[] files = di.GetFiles("*.*", SearchOption.AllDirectories);
             if (files != null && files.Length > 0)
             {
                 foreach (var file in files)
                 {
-                    if (file.Extension == (".jpg") ||
+                    if ((file.Extension == (".jpg") ||
                         file.Extension == (".png") ||
                         file.Extension == (".bmp") ||
-                        file.Extension == (".gif"))
+                        file.Extension == (".gif")) && !photosContain(file.FullName))
                     {
-                        photos.Add(new Photo()
+                        photos.Insert(0, new Photo()
                         {
                             FullPath = file.FullName
                         });
+                        //photos.Add();
                     }
                 }
             }
         }
 
-    }
+        private bool photosContain(string fullName)
+        {
+            foreach (var photo in photos)
+            {
+                if (photo.FullPath == fullName)
+                    return true;
+            }
+            return false;
+        }
 
-    
-    
+        private void setPhotoPath()
+        {
+
+        }
+
+        private void photosInit()
+        {
+            setPhotoPath();
+            DirectoryInfo di = new DirectoryInfo(photos_path);
+            FileInfo[] files = di.GetFiles("*.*", SearchOption.AllDirectories);
+            if (files != null && files.Length > 0)
+            {
+                foreach (var file in files)
+                {
+                    if ((file.Extension == (".jpg") ||
+                        file.Extension == (".png") ||
+                        file.Extension == (".bmp") ||
+                        file.Extension == (".gif")))
+                    {
+                        photos.Add(new Photo()
+                        {
+                            FullPath = file.FullName
+                        });
+                        //photos.Add();
+                    }
+                }
+            }
+            try
+            {
+                this.lstImg.ItemsSource = photos;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            };
+        }
+
+        private void updatePhotoes()
+        {
+            setPhotoPath();
+            getAllImagePath();
+            try
+            {
+                this.lstImg.ItemsSource = photos;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            };
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("连接成功");
+        }
+
+        private void Setting_Click(object sender, RoutedEventArgs e)
+        {
+            setting = new Setting();
+            setting.ShowDialog();
+            updatePhotoes();
+        }
+    }
 }
---
+++ easyShot/easyShot/Setting.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace easyShot
+{
+    /// <summary>
+    /// Setting.xaml 的交互逻辑
+    /// </summary>
+    /// 
+
+    public partial class Setting : Window
+    {
+
+        private string path;
+
+        public Setting()
+        {
+            InitializeComponent();
+            this.frmMain.Navigate(new Uri("./Pages/General.xaml", UriKind.Relative));
+        }
+
+        public void setPath(string path)
+        {
+            this.path = path;
+        }
+
+        private void BtnNav_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            this.frmMain.Navigate(new Uri("./Pages/" + btn.Tag.ToString() + ".xaml", UriKind.Relative));
+        }
+
+    }
+}
