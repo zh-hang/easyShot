32dcccd3b378e32852433c53aac11ca912c2e011
(buggyminer) <(liaojiudazhongbiao@outlook.com)>
2020-12-09 19:08:25 +0800

    完成了配置和云端接口。

--- easyShot/easyShot/Cloud.cs
+++ easyShot/easyShot/Cloud.cs
@@ -1,15 +1,178 @@
-﻿namespace easyShot
+﻿using Aliyun.OSS;
+using Aliyun.OSS.Common;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace easyShot
 {
-    class Cloud
+    enum CloudError{
+        ErrorNo,
+        ErrorTimeOut,
+        ErrorInvalidLink,
+        ErrorUserDoesNotExist,
+        ErrorIncorrectPassword
+    }
+
+    public class Cloud
     {
+        private EasyShot.ConfigManager config;
+        private string endpoint;
+        private string accessKeyId;
+        private string accessKeySecret;
+        private string bucketName;
+        private OssClient client;
+        private Bucket bucket;
+        private List<System.String> cloudFileList;
+        private List<System.String> localFileList;
+        public Cloud()
+        {
+            config = new EasyShot.ConfigManager();
+            cloudFileList = new List<string>();
+            loadConfig();
+            setUpConnect();
+            updateCloudFileList();
+            updateLocalFileList();
+        }
+        public void loadConfig()
+        {
+            this.endpoint = "oss-cn-beijing.aliyuncs.com";
+            this.accessKeyId = "LTAI4GL1UkZQoveLGWA49gmo";
+            this.accessKeySecret = "NWByphOyb5oj9U8q49ZDpe3H6PsU6K";
+            this.bucketName = "easyshot";
+        }
         //从config获取云端设置
-        public void upload()
+        public void setUpConnect()
+        {
+            // 创建OSSClient实例。
+            this.client = new OssClient(endpoint, accessKeyId, accessKeySecret);
+            try
+            {
+                if (!this.client.DoesBucketExist(this.bucketName))
+                {
+                    // 创建存储空间。
+                    this.bucket = client.CreateBucket(bucketName);
+                    System.Console.WriteLine("Create bucket succeeded, {0} ", bucket.Name);
+                }
+            }catch (System.Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                throw ex;
+            }
+        }
+        public void upLoad(System.String localFileName)
         {
             //这里需要网络异常处理机制
+            System.String objectName = localFileName;
+            localFileName = config.getshotfilepath()+@"\"+localFileName;
+            try
+            {
+                // 上传文件
+                client.PutObject(bucketName, objectName, localFileName);
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception("fail to uoload the file!");
+            }
         }
-        public void download()
+        public void downLoad(System.String filename)
         {
+
             //这里需要网络异常处理机制
+            try
+            {
+                // 下载文件到流。OssObject 包含了文件的各种信息，如文件所在的存储空间、文件名、元信息以及一个输入流。
+                System.String downloadFilename = this.config.getshotfilepath()+@"\"+filename;
+                OssObject obj = client.GetObject(bucketName, filename);
+                using (System.IO.Stream requestStream = obj.Content)
+                {
+                    byte[] buf = new byte[1024];
+                    var fs = System.IO.File.Open(downloadFilename, System.IO.FileMode.OpenOrCreate);
+                    var len = 0;
+                    // 通过输入流将文件的内容读取到文件或者内存中。
+                    while ((len = requestStream.Read(buf, 0, 1024)) != 0)
+                    {
+                        fs.Write(buf, 0, len);
+                    }
+                    fs.Close();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception("fail to download the file!");
+            }
         }
+        public void delete(System.String filename)
+        {
+            try
+            {
+                // 删除文件
+                client.DeleteObject(bucketName, filename);
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception("fail to delete the file!");
+            }
+        }
+
+        public void updateCloudFileList()
+        {
+            try
+            {
+                ListObjectsRequest listObjectsRequest = new ListObjectsRequest(bucketName) { MaxKeys = 100, };
+                ObjectListing result = client.ListObjects(listObjectsRequest);
+                foreach (OssObjectSummary summary in result.ObjectSummaries)
+                {
+                    this.cloudFileList.Add(summary.Key);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new System.Exception("fail to get the file list!");
+            }
+        }
+
+        public void updateLocalFileList()
+        {
+            System.String directorypath = config.getshotfilepath();
+            //List<string> dirs = new List<string>(System.IO.Directory.GetDirectories(directorypath, "*", System.IO.SearchOption.AllDirectories));
+            List<string> dirs = new List<string>();
+            System.IO.DirectoryInfo folder = new System.IO.DirectoryInfo(directorypath);
+            foreach (System.IO.FileInfo file in folder.GetFiles("*"))
+            {
+                dirs.Add(file.Name);
+            }
+            this.localFileList = dirs;
+        }
+        public void upLoadNewLocalFile()
+        {
+            foreach(var filename in localFileList)
+            {
+                if (cloudFileList.Find((System.String thisfilename)=> thisfilename.Equals(filename)) == null)
+                {
+                    upLoad(filename);
+                }
+            }
+        }
+        public void downLoadNewCloudFile()
+        {
+            foreach (var filename in cloudFileList)
+            {
+                if (localFileList.Find((System.String thisfilename) => thisfilename.Equals(filename)) == null)
+                {
+                    downLoad(filename);
+                }
+            }
+        }
+        public void deleteCloudFile()
+        {
+            foreach (var filename in cloudFileList)
+            {   
+                if (localFileList.Find((System.String thisfilename) => thisfilename.Equals(filename)) == null)
+                {
+                    delete(filename);
+                }
+            }
+        }
+
     }
 }
--- easyShot/easyShot/ConfigureManager.cs
+++ easyShot/easyShot/ConfigureManager.cs
@@ -16,6 +16,18 @@ namespace EasyShot
     //为了便于进行单元测试，暂时将这些类设为公共类
     public class ConfigManager
     {
+        /*
+        需要保存如下属性：
+        文件路径
+        截图模式
+        启动模式
+        服务器地址
+        账户
+        密码
+        服务器密钥
+        */
+
+
         //将xml属性转换为程序中使用的枚举变量
         private const string startautomaticallty = "startautomaticallty";
         private const string startmanually = "startmanually";
@@ -27,6 +39,9 @@ namespace EasyShot
         private const string shotfilepathlabel = "//configuration/shotfilepath";
         private const string startmodelabel = "//configuration/startmode";
         private const string shotmodelabel = "//configuration/shotmode";
+        private const string serveraddresslabel = "//configuration/serveraddress";
+        private const string serveraccountlabel = "//configuration/serveraccount";
+        private const string serverpasswordlabel = "//configuration/serverpassword";
 
         private System.Xml.XmlDocument configXmlFile;
         private System.Xml.XmlNode root;
@@ -34,6 +49,10 @@ namespace EasyShot
         private string configXmlFilePath;
         private StartMode startMode;
         private ShotMode shotMode;
+        private string serverAddress;
+        private string serverAccount;
+        private string serverPassword;
+
         public ConfigManager()
         {
             loadconfigXmlFilePath();
@@ -128,6 +147,63 @@ namespace EasyShot
                 element.SetAttribute("content", ConfigManager.shotwindow);
             else
                 element.SetAttribute("content", ConfigManager.shotsquare);
+        }
+        public void loadServerAddress()
+        {
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serveraddresslabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            this.serverAddress = element.GetAttribute("content").ToString();
+        }
+        public string getServerAddress()
+        {
+            return this.serverAddress;
+        }
+        public void setServerAddress(string newServerAddress)
+        {
+            this.serverAddress = newServerAddress;
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serveraddresslabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            element.SetAttribute("content", newServerAddress);
+        }
+        public void loadServerAccount()
+        {
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serveraccountlabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            this.serverAccount = element.GetAttribute("content").ToString();
+        }
+        public string getServerAccount()
+        {
+            return this.serverAccount;
+        }
+        public void setServerAccount(string newServerAccount)
+        {
+            this.serverAccount = newServerAccount;
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serveraccountlabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            element.SetAttribute("content", newServerAccount);
+        }
+        public void loadServerPassword()
+        {
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serverpasswordlabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            this.serverPassword = element.GetAttribute("content").ToString();
+        }
+        public string getServerPassword()
+        {
+            return this.serverPassword;
+        }
+        public void setServerPassword(string newServerpassword)
+        {
+            this.serverPassword = newServerpassword;
+            System.Xml.XmlNode node = this.configXmlFile.SelectSingleNode(serverpasswordlabel);
+            if (node == null) throw new System.Exception("the Configure file is illegal!");
+            System.Xml.XmlElement element = (System.Xml.XmlElement)node;
+            element.SetAttribute("content", newServerpassword);
         }
     }
 }
